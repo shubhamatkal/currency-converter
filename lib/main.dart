6923635273b6967ddc795e52375f7f394bbe867f
shubhamatkal <shubhamatkal333@gmail.com>
2024-05-02 15:56:13 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Hello, World!!!!',
+      textDirection: TextDirection.ltr,
+    );
+  }
+}
